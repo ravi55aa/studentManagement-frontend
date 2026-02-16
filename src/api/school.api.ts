e0ac1f1ab076c1ff8dfbe754e88e9e75f20bae22
Ravi <rachouhan58@gmail.com>
2026-02-16 15:43:47 +0530

    refactor(school.api): move redux code from api layer

--- src/api/school.api.ts
+++ src/api/school.api.ts
@@ -79,14 +79,10 @@ export const handleDocsUploadCreateSchoolApi =
     export const handleSchoolSignIn=async(payload:object)=>{
         try{
 
-            console.log(payload);
             const response=await axiosBaseURL.get("/school/register",{
                 params:payload,
                 headers:{role:"Admin"}
             });
-            const school=response.data;
-            localStorage.setItem("authUserId",school._id);
-            localStorage.setItem("authUserModel","School");
 
             return {success:true,data:response?.data};
         } catch(error){
